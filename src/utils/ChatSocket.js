2e4c198ef7d3c66a9d1132c56cba0fba6853f471
developers_zone <developers.email@test.com>
2025-07-03 09:31:52 +0530

    setup project

---
+++ src/utils/ChatSocket.js
@@ -0,0 +1,130 @@
+import { io } from "socket.io-client";
+
+class ChatSocket {
+  constructor(serverUrl, userId) {
+    this.serverUrl = serverUrl;
+    this.userId = userId;
+    this.socket = null;
+    this.messages = [];
+
+    // Callback storage
+    this.callbacks = {
+      handshakeSuccess: null,
+      messageReceived: null,
+      messageSent: null,
+      retrieveMessages: null,
+      chatList: null,
+      messageUpdate: null,
+    };
+
+    this.initializeSocket();
+  }
+
+  initializeSocket() {
+    this.socket = io(this.serverUrl);
+
+    // Emit register event with userId
+    this.socket.emit("register", this.userId);
+
+    // Listen for all socket events and trigger corresponding callbacks
+
+    this.socket.on("receive_message", (message) => {
+      this.messages.push(message);
+      if (this.callbacks.messageReceived) {
+        this.callbacks.messageReceived(message);
+      }
+    });
+
+    this.socket.on("handshake_success", (data) => {
+      if (this.callbacks.handshakeSuccess) {
+        this.callbacks.handshakeSuccess(data);
+      }
+    });
+
+    this.socket.on("retrieve_message", (data) => {
+      if (this.callbacks.retrieveMessages) {
+        this.callbacks.retrieveMessages(data);
+      }
+    });
+
+    this.socket.on("message_sent", (data) => {
+      if (this.callbacks.messageSent) {
+        this.callbacks.messageSent(data);
+      }
+    });
+
+    this.socket.on("chatlist", (data) => {
+      if (this.callbacks.chatList) {
+        this.callbacks.chatList(data);
+      }
+    });
+
+    this.socket.on("message_update", (data) => {
+      if (this.callbacks.messageUpdate) {
+        this.callbacks.messageUpdate(data);
+      }
+    });
+  }
+
+  sendMessage(received, message) {
+    if (this.socket) {
+      this.socket.emit("send_message", {
+        receiverId: received,
+        message,
+      });
+    }
+  }
+
+  joinChat(receiverId) {
+    if (this.socket) {
+      this.socket.emit("joinChat", { receiverId });
+    }
+  }
+
+  getMessages() {
+    return this.messages;
+  }
+
+  // ===============================
+  // === Callback Registration APIs
+  // ===============================
+  setHandshakeSuccessCallback(callback) {
+    this.callbacks.handshakeSuccess = callback;
+  }
+
+  setMessageReceivedCallback(callback) {
+    this.callbacks.messageReceived = callback;
+  }
+
+  setMessageSentCallback(callback) {
+    this.callbacks.messageSent = callback;
+  }
+
+  setRetrieveMessagesCallback(callback) {
+    this.callbacks.retrieveMessages = callback;
+  }
+
+  setChatListCallback(callback) {
+    this.callbacks.chatList = callback;
+  }
+
+  setMessageUpdateCallback(callback) {
+    this.callbacks.messageUpdate = callback;
+  }
+
+  get methods() {
+    return {
+      chatSocket: this,
+      sendMessage: this.sendMessage.bind(this),
+      getMessages: this.getMessages.bind(this),
+      setHandshakeSuccessCallback: this.setHandshakeSuccessCallback.bind(this),
+      setMessageReceivedCallback: this.setMessageReceivedCallback.bind(this),
+      setMessageSentCallback: this.setMessageSentCallback.bind(this),
+      setRetrieveMessagesCallback: this.setRetrieveMessagesCallback.bind(this),
+      setChatListCallback: this.setChatListCallback.bind(this),
+      setMessageUpdateCallback: this.setMessageUpdateCallback.bind(this),
+    };
+  }
+}
+
+export default ChatSocket;
